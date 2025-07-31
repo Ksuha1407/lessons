4794e5bef0440b46ad3d1a0ede80a2c85f4840c5
Oksana Nikitina <ksuha14071973@gmail.com>
2025-07-31 23:04:58 +0300

    Виправила помилки

--- ecobazar/js/script.js
+++ ecobazar/js/script.js
@@ -1,101 +1,54 @@
-"use strict";
+"use strict"
 
-window.addEventListener("load", windowLoad)
+window.addEventListener('load', windowLoad)
 
 let isMobile
-function isMobileDevice() {
-    return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
-}
 
 function windowLoad() {
-    // Додаємо атрибут data-touch на мобільних
     isMobile = window.navigator.userAgentData.mobile
-    isMobile ? document.body.setAttribute(`data-touch`, ``) : null
+    isMobile ? document.body.setAttribute('data-touch', '') : null
 
-    document.addEventListener("click", documentActions)
+    document.addEventListener('click', documentActions)
 
-    const coundown = document.querySelectorAll(`[data-coundown]`)
+    const coundown = document.querySelectorAll('[data-coundown]')
     coundown.length ? initCoundown(coundown) : null
 
-
-    dynamicAdapHeader()
+    dynamicAdaptHeader();
 }
+function dynamicAdaptHeader() {
+    const topHeader = document.querySelector('.top-header')
+    const header = document.querySelector('.header')
+    const menu = document.querySelector('.menu')
 
-// Бургер та меню
-function documentActions(e) {
-    const targetElement = e.target
+    const phoneHeader = document.querySelector('.bottom-header__phone')
+    const searchHeader = document.querySelector('.search-header')
 
-    // Клік по бургеру
-    if (targetElement.closest(".icon-menu")) {
-        document.documentElement.classList.toggle("open-menu")
-        document.body.classList.toggle("scroll-lock")
-    }
-
-    // Клік по кнопці підменю на мобілках
-    if (isMobile) {
-        if (targetElement.closest(".menu__button")) {
-            const menuItem = targetElement.closest(".menu__item")
-            const isActive = menuItem.classList.contains("--active")
-
-            document.querySelectorAll(".menu__item.--active").forEach(item => {
-                item.classList.remove("--active")
-            });
-
-            if (!isActive) {
-                menuItem.classList.add("--active")
-            }
-        } else {
-            document.querySelectorAll(".menu__item.--active").forEach(item => {
-                item.classList.remove("--active")
-            })
-        }
-    }
-}
-
-// Динамічне переміщення елементів в хедері
-function dynamicAdapHeader() {
-    const topHeader = document.querySelector(".top-header")
-    const menu = document.querySelector(".menu")
-    const header = document.querySelector(".header")
-    const phoneHeader = document.querySelector(".bottom-header__phone")
-    const searchHeader = document.querySelector(".search-header")
-    const bottomContainer = document.querySelector(".bottom-header__container")
-    const actionsHeader = document.querySelector(".actions-header")
+    const bottomContainer = document.querySelector('.bottom-header__container')
+    const actionsHeader = document.querySelector('.actions-header')
+    const placeSearch = document.querySelector('.body-header__place-search')
 
     const media = window.matchMedia("(max-width: 767.98px)")
 
-    media.addEventListener("change", () => {
-        dynamicAdapHeaderInit(media)
-    });
+    media.addEventListener("change", (e) => {
+        dynamicAdaptHeaderInit(media)
+    })
+    dynamicAdaptHeaderInit(media)
 
-    dynamicAdapHeaderInit(media)
-
-    function dynamicAdapHeaderInit(media) {
+    function dynamicAdaptHeaderInit(media) {
         if (media.matches) {
-            bottomContainer.insertAdjacentElement("beforeend", searchHeader)
-            actionsHeader.insertAdjacentElement("beforeend", phoneHeader)
-            menu.insertAdjacentElement("beforeend", topHeader)
+            bottomContainer.insertAdjacentElement('beforeend', searchHeader)
+            actionsHeader.insertAdjacentElement('beforeend', phoneHeader)
+            menu.insertAdjacentElement('beforeend', topHeader)
         } else {
-            bottomContainer.insertAdjacentElement("beforeend", phoneHeader)
-            actionsHeader.insertAdjacentElement("beforeBegin", searchHeader)
-            header.insertAdjacentElement("afterBegin", topHeader)
-        }
+            bottomContainer.insertAdjacentElement('beforeend', phoneHeader)
+            placeSearch.insertAdjacentElement('beforeend', searchHeader)
 
-        searchHeader.classList.toggle("--dynamic", media.matches)
-        phoneHeader.classList.toggle("--dynamic", media.matches)
+            header.insertAdjacentElement("afterbegin", topHeader)
+        }
+        searchHeader.classList.toggle('--dynamic', media.matches)
+        phoneHeader.classList.toggle('--dynamic', media.matches)
     }
 }
-
-// Footer акордеон
-document.querySelectorAll(".menus-footer__title").forEach(title => {
-    title.addEventListener("click", () => {
-        const list = title.nextElementSibling
-        title.classList.toggle("active")
-        list.classList.toggle("active")
-    })
-})
-
-
 function documentActions(e) {
     const targetElement = e.target
     if (isMobile) {
@@ -154,8 +107,6 @@ function flyImage(productImage, cartHeader) {
     }, speed);
 }
 
-
-
 function initCoundown(coundown) {
     coundown.forEach(coundownItem => {
         initCoundownItem(coundownItem)
@@ -188,4 +139,12 @@ function initCoundownItem(coundownItem) {
 }
 
 
+// Footer акордеон
+document.querySelectorAll(".menus-footer__title").forEach(title => {
+    title.addEventListener("click", () => {
+        const list = title.nextElementSibling
+        title.classList.toggle("active")
+        list.classList.toggle("active")
+    })
+})
 
